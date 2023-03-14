c54370739c471090a035f0bb4d7034c8c6924de3
SumonRayy <sumonrayy@gmail.com>
2023-03-14 23:57:46 +0530

    ⬆️ updated : urls in config

--- config.ts
+++ config.ts
@@ -1,18 +1,17 @@
 import ConfigModel from "./models/configModel";
 
- 
-const dbConnection:string = 'default';
+const dbConnection: string = "default";
 const PRECISION: number = +process.env.TKN_PRCS;
 
-export default new ConfigModel({ 
-    EVMSSocketUrl:'wss://mainnet.telos.caleos.io/evm',
-    EVMUrl:'https://mainnet.telos.caleos.io/evm',
-    hyperionUrl:'https://mainnet.telos.caleos.io',
-    oracleAccount: process.env.NTV_ACCNT,
-    oraclePrivateKey: process.env.NTV_PK,
-    precision: PRECISION,
-    symbol: process.env.TKN_SYMB,
-    contract: process.env.TKN_CNTRCT,
-    EVMContract: process.env.EVM_CA,
-    EVMContractPrivateKey: process.env.EVM_PK
+export default new ConfigModel({
+  EVMSSocketUrl: "wss://telos.caleos.io/evm",
+  EVMUrl: "https://telos.caleos.io/evm",
+  hyperionUrl: "https://telos.caleos.io",
+  oracleAccount: process.env.NTV_ACCNT,
+  oraclePrivateKey: process.env.NTV_PK,
+  precision: PRECISION,
+  symbol: process.env.TKN_SYMB,
+  contract: process.env.TKN_CNTRCT,
+  EVMContract: process.env.EVM_CA,
+  EVMContractPrivateKey: process.env.EVM_PK,
 });
